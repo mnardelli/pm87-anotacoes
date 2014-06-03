068aca59e38c6b1952b38c00979735d6560a8821
Marcelo Nardelli <nardellis@yahoo.com.br>
2014-06-03 09:39:18 -0300

    Segunda alteracao

--- TesteGit.java
+++ TesteGit.java
@@ -2,4 +2,8 @@ public class TesteGit {
 	public static void main(String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
 	}
+
+	public static String testa(String msgBase) {
+		return msgBase + "teste";
+	}
 }
